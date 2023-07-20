5ba018f244603bcb7dcc7c901d099dc97a1cd115
sdhkj <823188970@qq.com>
2023-07-20 22:14:24 +0800

    修正

--- album/album/settings.py
+++ album/album/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,4 +141,4 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = Path(BASE_DIR).joinpath('collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
